cadd89d81a5d1167b39dd6aab24e916b1f900e39
Ehrman <Ashley.Ehrman@dfo-mpo.gc.ca>
2021-06-23 17:21:30 -0500

    removed key

--- authentication.R
+++ authentication.R
@@ -5,7 +5,6 @@ library(gitcreds)
 
 #This will take you you a github website where it will automatically select the scope
 #and you can ask it to generate a token. COPY this token!
-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
 
 usethis::create_github_token()
 
